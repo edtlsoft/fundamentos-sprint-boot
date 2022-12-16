d781e30b92fd281f38ef9e488d04bb545e16d18c
Edward Trigos <edward.trigos@nivelics.co>
2022-12-16 16:02:26 -0500

    Initial commit

---
+++ src/main/java/com/edtlsoft/sprintboot/edtlsoft/EdtlsoftApplication.java
@@ -0,0 +1,13 @@
+package com.edtlsoft.sprintboot.edtlsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdtlsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdtlsoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edtlsoft/sprintboot/edtlsoft/EdtlsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edtlsoft.sprintboot.edtlsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdtlsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
